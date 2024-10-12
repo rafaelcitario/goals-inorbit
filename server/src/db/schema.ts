42addf3618ba5fe36aaba646a7b63feaf257d3cb
Rafael Citario <contato.rafaelgomes@outlook.com>
2024-10-12 18:26:43 -0300

    create new table on DB

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,15 +1,9 @@
 import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core';
+import { randomUUID } from 'node:crypto';
 
 export const goalsTable = pgTable('goals', {
-  id: text('id').primaryKey(),
+  id: text('id').primaryKey().$defaultFn(() => randomUUID()),
   title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow()
-});
-
-export const completedGoals = pgTable('completed_goals', {
-  id: text('completed_goals_id').primaryKey().notNull(),
-  goalId: text('goal_id').references(() => goalsTable.id).notNull(),
-  title: text('completed_goals_title').notNull(),
-  completedAt: timestamp('completed_goals_at', { withTimezone: true }).notNull().defaultNow()
 });
